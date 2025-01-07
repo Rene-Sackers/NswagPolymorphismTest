e05003e6ece93d9e346ca0a5ddb6b18a2fedc9ab
Rene Sackers <rene.sackers@beterbed.nl>
2025-01-07 11:51:43 +0100

    Update client

--- InheritanceExample.Client/Generated/Client.cs
+++ InheritanceExample.Client/Generated/Client.cs
@@ -150,7 +150,7 @@ namespace InheritanceExample.Client
 
         /// <returns>OK</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual System.Threading.Tasks.Task PostAsync(System.Collections.Generic.IEnumerable<Dog> body)
+        public virtual System.Threading.Tasks.Task PostAsync(Dog body)
         {
             return PostAsync(body, System.Threading.CancellationToken.None);
         }
@@ -158,7 +158,7 @@ namespace InheritanceExample.Client
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <returns>OK</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual async System.Threading.Tasks.Task PostAsync(System.Collections.Generic.IEnumerable<Dog> body, System.Threading.CancellationToken cancellationToken)
+        public virtual async System.Threading.Tasks.Task PostAsync(Dog body, System.Threading.CancellationToken cancellationToken)
         {
             var client_ = _httpClient;
             var disposeClient_ = false;
@@ -176,82 +176,6 @@ namespace InheritanceExample.Client
                     if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
                     // Operation Path: "Post"
                     urlBuilder_.Append("Post");
-
-                    PrepareRequest(client_, request_, urlBuilder_);
-
-                    var url_ = urlBuilder_.ToString();
-                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
-
-                    PrepareRequest(client_, request_, url_);
-
-                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
-                    var disposeResponse_ = true;
-                    try
-                    {
-                        var headers_ = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>();
-                        foreach (var item_ in response_.Headers)
-                            headers_[item_.Key] = item_.Value;
-                        if (response_.Content != null && response_.Content.Headers != null)
-                        {
-                            foreach (var item_ in response_.Content.Headers)
-                                headers_[item_.Key] = item_.Value;
-                        }
-
-                        ProcessResponse(client_, response_);
-
-                        var status_ = (int)response_.StatusCode;
-                        if (status_ == 200)
-                        {
-                            return;
-                        }
-                        else
-                        {
-                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
-                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
-                        }
-                    }
-                    finally
-                    {
-                        if (disposeResponse_)
-                            response_.Dispose();
-                    }
-                }
-            }
-            finally
-            {
-                if (disposeClient_)
-                    client_.Dispose();
-            }
-        }
-
-        /// <returns>OK</returns>
-        /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual System.Threading.Tasks.Task TestAsync(Dog body)
-        {
-            return TestAsync(body, System.Threading.CancellationToken.None);
-        }
-
-        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>OK</returns>
-        /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual async System.Threading.Tasks.Task TestAsync(Dog body, System.Threading.CancellationToken cancellationToken)
-        {
-            var client_ = _httpClient;
-            var disposeClient_ = false;
-            try
-            {
-                using (var request_ = new System.Net.Http.HttpRequestMessage())
-                {
-                    var json_ = Newtonsoft.Json.JsonConvert.SerializeObject(body, JsonSerializerSettings);
-                    var content_ = new System.Net.Http.StringContent(json_);
-                    content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json-patch+json");
-                    request_.Content = content_;
-                    request_.Method = new System.Net.Http.HttpMethod("POST");
-
-                    var urlBuilder_ = new System.Text.StringBuilder();
-                    if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
-                    // Operation Path: "Test"
-                    urlBuilder_.Append("Test");
 
                     PrepareRequest(client_, request_, urlBuilder_);
 
--- InheritanceExample.ClientConsumer/Program.cs
+++ InheritanceExample.ClientConsumer/Program.cs
@@ -4,12 +4,7 @@ using Newtonsoft.Json;
 using var httpClient = new HttpClient();
 var animalsClient = new Client("https://localhost:7235/", httpClient);
 
-var gotAnimals = await animalsClient.GetAnimalsAsync();
+var gotAnimals = await animalsClient.GetAsync();
 Console.WriteLine(JsonConvert.SerializeObject(gotAnimals, Formatting.Indented));
 
-var postAnimals = new Animal[]
-{
-	new Dog {LegCount = 4, TailLength = 30},
-	new Spider { LegCount = 8, Venomous = true}
-};
-await animalsClient.PostAnimalsAsync(postAnimals);
+await animalsClient.PostAsync(new Spider {LegCount = 8, Venomous = true });
